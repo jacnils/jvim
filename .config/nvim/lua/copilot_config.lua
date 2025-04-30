fcb50343667514375269fe3827c46bf27a810c34
Jacob Nilsson <jacob@jacobnilsson.com>
2025-04-30 20:20:56 +0200

    jvim | Add new config

---
+++ .config/nvim/lua/copilot_config.lua
@@ -0,0 +1,7 @@
+--[[
+            jacob's neovim configuration
+    -- https://github.com/jacnils/jacob-nvim --
+]]--
+
+require("copilot").setup({})
+require("CopilotChat").setup({})
--- commit.sh
+++ commit.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ -e "$HOME/.config/nvim" ]; then
-    rm -rf .config/nvim
+    rm -rf .config
     mkdir -p .config
     cp -r $HOME/.config/nvim .config/
     rm -f .config/nvim/.session.nvim
     mv .config/nvim/lazy-lock.json .config/nvim/.lazy-lock.json
 
-    git add .config/* screenshots/ commit.sh install.sh
+    git add .config commit.sh install.sh
     git commit -a -m "jvim | Add new config"
     git push
 fi
